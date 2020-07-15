075493dd486b3915b178b4f999b02f3f1a5982c5
vojajokic <v.jokic@hotmail.com>
2020-07-15 13:56:17 +0200

    2 files modified, 1 added

--- src/app/customers/customers-routing.module.ts
+++ src/app/customers/customers-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { CustomerListComponent } from './customer-list/customer-list.component';
+import { CustomerListComponent } from './customer-list/customer-list.component'; 
+import { CustomerCardComponent } from './customer-card/customer-card.component';
 
 const routes: Routes = [
   {
     path: '',
     component: CustomerListComponent
+  },
+  {
+    path: 'info',
+    component: CustomerCardComponent
   }
   ];
 
--- src/app/customers/customers.module.ts
+++ src/app/customers/customers.module.ts
@@ -3,16 +3,17 @@ import { CommonModule } from '@angular/common';
 
 import { CustomersRoutingModule } from './customers-routing.module';
 import { CustomerListComponent } from './customer-list/customer-list.component';
+import { CustomerCardComponent } from './customer-card/customer-card.component';
 
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card'; 
 import { MatIconModule } from '@angular/material/icon';
-import { CustomerCardComponent } from './customer-card/customer-card.component';
 
 @NgModule({
   imports: [
     CommonModule,
-    CustomersRoutingModule,
+    CustomersRoutingModule, 
+    CustomerCardComponent,
     //Material
     MatButtonModule,
     MatCardModule,
